94282ad1cfdf511be07acca92b729be119eed68f
Adam Fowler <lilglower@hotmail.com>
2022-04-25 22:34:36 -0600

    Fix slash direction

--- tools/findTitanfallLocation.ts
+++ tools/findTitanfallLocation.ts
@@ -1,9 +1,9 @@
 import { promises as fsPromises } from 'fs';
 
 const locations: string[] = [
-    `C:\Program Files (x86)\Origin Games\Titanfall2`,
-    `C:\Program Files (x86)\Steam\steamapps\common\Titanfall2`,
-    `C:\Program Files\EA Games\Titanfall2`
+    `C:/Program Files (x86)/Origin Games/Titanfall2`,
+    `C:/Program Files (x86)/Steam/steamapps/common/Titanfall2`,
+    `C:/Program Files/EA Games/Titanfall2`
 ];
 
 export default async function findTitanfallLocation():Promise<string> {
